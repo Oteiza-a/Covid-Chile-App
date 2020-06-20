aa15aca64141f0dc53ccb67bab24aac3bbd5933d
Alberto Oteiza <alberto.jesus.oteiza@gmail.com>
2020-06-20 19:45:10 -0400

    added the heroku port

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
 const app = express();
 
 app.use(express.json());
-
 app.use(express.static('public'));
+
+var port = porcess.env.port || 5000;
 
 app.listen(3000, () => {
     console.log('App listening on port 3000');
