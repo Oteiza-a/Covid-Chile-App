30d5cd0cba8913eb5ecc25a0bb32f664bb452ba2
Alberto Oteiza <alberto.jesus.oteiza@gmail.com>
2020-06-24 17:02:27 -0400

    adding favicon

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const favicon = require('serve-favicon');
 
+app.use(favicon('./public/images/favicon.ico'));
 app.use(express.json());
 app.use(express.static('public'));
 
